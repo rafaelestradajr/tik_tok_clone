23a526a59b9aa0edb90d3001352e4a471ac4986c
Rafael Estrada <rafaelestrada@comcast.net>
2021-08-10 21:36:15 -0700

    initial commit

---
+++ src/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <h1>this is a video</h1>
+    </div>
+  );
+}
+
+export default Video;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAqdwCbSJhUIJjT_BpItSfFNrJWGEcR7L8",
+    authDomain: "tiktok-clone-bdda4.firebaseapp.com",
+    projectId: "tiktok-clone-bdda4",
+    storageBucket: "tiktok-clone-bdda4.appspot.com",
+    messagingSenderId: "964084155488",
+    appId: "1:964084155488:web:d294adfdf367e5cfdeaf05",
+    measurementId: "G-XEVQ24N6Q4"
+  };
