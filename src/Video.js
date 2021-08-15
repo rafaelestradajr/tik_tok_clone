0b236afd7c47de2881c59f911dc931ca34ef127e
Rafael Estrada <rafaelestrada@comcast.net>
2021-08-14 22:15:47 -0700

    initial commit

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
 import "./Video.css";
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="video">
-      <h1>this is a video</h1>
+      <video
+        className="video__player"
+        loop
+        onClick={onVideoPress}
+        ref={videoRef}
+        src={url}
+      ></video>
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSidebar likes={likes} messages={messages} shares={shares} />
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAqdwCbSJhUIJjT_BpItSfFNrJWGEcR7L8",
@@ -8,3 +10,8 @@ const firebaseConfig = {
     appId: "1:964084155488:web:d294adfdf367e5cfdeaf05",
     measurementId: "G-XEVQ24N6Q4"
   };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  
+  export default db;
